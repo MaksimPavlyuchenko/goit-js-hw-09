66989dcc29e874254dc6a6619ec69daae62b3aed
MaksimPavlyuchenko <maksimpavlyuchenko82@gmail.com>
2023-04-16 14:14:52 +0300

    first-task-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const start = document.querySelector(['button[data-start]']);
+const stop = document.querySelector(['button[data-stop]']);
+const body = document.querySelector('body');
 
+let bodyColor = body.style.backgroundColor;
+
+start.addEventListener('click', () => {
+  bodyColor = setInterval(changeColor, 1000);
+  start.disabled = 'true';
+});
+stop.addEventListener('click', () => {
+  clearInterval(bodyColor);
+  start.disabled = '';
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
